0491e1d3b5d5ee95988cddd3ace2489ce2a05193
2789356h <2789356h@student.gla.ac.uk>
2025-04-24 20:26:17 +0100

    Updated Navbar for Mobile Devices
    navbar options menu shows when on mobile devices, nav bar displayed on desktop

--- iipex-webapp/src/components/Navbar.jsx
+++ iipex-webapp/src/components/Navbar.jsx
@@ -11,14 +11,14 @@ function Navbar() {
   return (
     <div className="max-w-[1240px] mx-auto px-4 text-white">
         <h1 className='w-full text-3xl font-bold text-[#00df91]'>IIPEX Accounting Services</h1>
-        <ul className='flex justify-between items-center'>
+        <ul className='justify-between items-center hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>About</li>
             <li className='p-4'>Services</li>
             <li className='p-4'>Bespoke</li>
             <li className='p-4'>Contact Us</li>
         </ul>
-        <div onClick={handleNav} className='fixed top-4 right-4 z-10 cursor-pointer hidden'>
+        <div onClick={handleNav} className='fixed top-4 right-4 z-10 cursor-pointer block md:hidden'>
             {!nav ? <AiOutlineMenu onClick={handleNav} size={20} /> : <AiOutlineClose onClick={handleNav} size={20} />}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out fixed left-[-100%]'}>
