3b0365e5af0a16625dc36ac6fe06eb100cdfbce9
2789356h <2789356h@student.gla.ac.uk>
2025-04-24 21:55:35 +0100

    Added Info Performance to Homepage
    Homepage has info performance section

--- iipex-webapp/src/App.js
+++ iipex-webapp/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import InfoPerformance from './components/InfoPerformance';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <InfoPerformance />
     </div>
   );
 }
