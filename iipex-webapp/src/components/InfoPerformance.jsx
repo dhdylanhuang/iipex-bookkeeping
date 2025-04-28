031f69b174f57a6a0bd2aa6d8efb31aaea15322f
2789356h <2789356h@student.gla.ac.uk>
2025-04-28 20:11:14 +0100

    Info Performance

--- iipex-webapp/src/components/InfoPerformance.jsx
+++ iipex-webapp/src/components/InfoPerformance.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import fsb_logo from '../assets/fsb-logo.png';
+import aat_logo from '../assets/aat-logo.jpg';
 
 const InfoPerformance = () => {
   return (
@@ -6,13 +8,25 @@ const InfoPerformance = () => {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
         <div className='flex flex-col justify-center'>
           <p className='text-xl text-blue-500 font-bold'>INFO PERFORMANCE</p>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Below is all about out INFO Performance</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            molestiae delectus culpa hic assumenda, voluptate reprehenderit
-            dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
-            eveniet ex deserunt fuga?
-          </p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
+            We are Members of and Certified with:
+          </h1>
+          <div className="flex flex-row space-x-8 mt-4">
+            <a href="https://www.fsb.org.uk/">
+              <img
+                src={fsb_logo}
+                alt="FSB Logo"
+                className="mt-4 w-56 transform transition-transform duration-300 ease-in-out hover:scale-110"
+              />
+            </a>
+            <a href="https://www.aat.org.uk/about">
+              <img
+                src={aat_logo}
+                alt="AAT Logo"
+                className="mt-0 w-56 transform transition-transform duration-300 ease-in-out hover:scale-110"
+              />
+            </a>
+          </div>
           {/* <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button> */}
         </div>
       </div>
